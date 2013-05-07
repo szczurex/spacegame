9c6c0def187bd5668e3cdf5f638f1b4e9c111180
Łukasz Zarzecki <lukasz.zarzecki@fwioo.pl>
2013-05-07 14:59:24 +0200

    part name-generating WIP

--- items/parts.py
+++ items/parts.py
@@ -1,6 +1,16 @@
 from items import GameItem
 from items import TYPE_RESCUE_POD_PART
 
+# might be used for part name-generating.
+PREFIXES = ['Alpha','Beta','Gamma','Delta','Epsilon',
+            'Zeta','Eta','Theta','Iota','Kappa','Lambda',
+            'Mu','Nu','Xi','Omicron','Pi','Rho','Sigma',
+            'Tau','Upsilon','Phi','Chi','Psi','Omega']
+
+INFIXES = ['Power','Core','Generator','Engine',
+           'Plasma','Steam','Air']
+
+SUFFIX = ['Module','Mount','Valve']
 
 class RescuePodPart(GameItem):
     name = 'Item name'
